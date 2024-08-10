93979591de3518aea1761012377bcf1155cd9cac
Ayeshabasharatrandhawa <ayeshabasharatrandhawa@gmail.com>
2024-08-10 07:55:16 -0700

    Add files via upload

---
+++ ayesha/script.js
@@ -0,0 +1,15 @@
+let head = document.getElementById('head')
+console.log(head);
+let heading = document.getElementsByClassName('heading-class');
+console.log(heading);
+//let paras = document.getElementsByTagName('p');
+//console.log(paras);
+
+
+let firstelement2 = document.querySelectorAll ('p');
+console.log(firstelement2);
+let h1 = document.querySelector ('h1');
+console.log(h1.innerText);
+h1.innerText = h1.innerText + "from  Ayesha"
+let divs = document.querySelectorAll('divs');
+console.log('divs');
